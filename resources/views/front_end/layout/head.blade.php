5b80a7a03bed3e1d07879ab6248fe71f19f28273
karan <kn@int.iqsay.com>
2025-01-14 13:51:33 +0530

    fixed

--- resources/views/front_end/layout/head.blade.php
+++ resources/views/front_end/layout/head.blade.php
@@ -4,7 +4,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">    
-    <title>{{ @yield('title', 'Jour De Match') }}</title>
+    <title></title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css"
         integrity="sha512-5Hs3dF2AEPkpNAR7UiOHba+lRSJNeM2ECkwxUIxC1Q/FLycGTbNapWXB4tP889k5T5Ju8fs4b1P5z/iB4nMfSQ=="
         crossorigin="anonymous" referrerpolicy="no-referrer">
