c0ad8ada72e42e3c4430b25f359e008316fc0d8c
karan <kn@int.iqsay.com>
2025-01-14 17:27:05 +0530

    fixed

--- resources/views/front_end/auth/signup.blade.php
+++ resources/views/front_end/auth/signup.blade.php
@@ -1,5 +1,5 @@
 @extends('front_end.layout.main')
-@section('title', 'S'inscrire')
+@section('title', 'S`inscrire')
 @section('content')
 <!-- breadcrumb start -->
 <div class="breadcrumb-area bg-overlay" style="background-image:url('assets/img/banner/leagues-pages-banner.jpg')">
