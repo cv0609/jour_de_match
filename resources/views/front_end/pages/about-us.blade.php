a524bd3226e32b7355099e6a8687aa175528e779
karan <kn@int.iqsay.com>
2025-01-14 13:42:12 +0530

    fixed

--- resources/views/front_end/pages/about-us.blade.php
+++ resources/views/front_end/pages/about-us.blade.php
@@ -1,4 +1,5 @@
 @extends('front_end.layout.main')
+@section('tittle','About us')
 @section('content')
 <!-- breadcrumb start -->
 <div class="breadcrumb-area bg-overlay" style="background-image:url('assets/img/banner/about-banner.jpg')">
