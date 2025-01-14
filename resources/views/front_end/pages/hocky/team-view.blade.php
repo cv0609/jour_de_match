5874f58a37c2f542007cc7fdb8e8dd6d0b95e405
karan <kn@int.iqsay.com>
2025-01-14 17:57:09 +0530

    fixed

--- resources/views/front_end/pages/hocky/team-view.blade.php
+++ resources/views/front_end/pages/hocky/team-view.blade.php
@@ -90,7 +90,7 @@
                     @foreach($myTeam as $key => $team)
                            <li>
                             <div class="player-dp captain-width">
-                                <img src="{{ $team['team_logo'] }}" alt="dp">
+                                <img src="" alt="dp">
                             </div>
                             <div class="player-info captain-width">
                                 <h6>{{ $team['player_name'] }}</h6>
