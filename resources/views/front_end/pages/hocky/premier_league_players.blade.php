c404189afa974d91558f1e29f7b793d979d9494d
karan <kn@int.iqsay.com>
2025-01-15 11:58:44 +0530

    fixed

--- resources/views/front_end/pages/hocky/premier_league_players.blade.php
+++ resources/views/front_end/pages/hocky/premier_league_players.blade.php
@@ -32,7 +32,7 @@
 
         <div class="basket_leagues-inner basket_leagues-inner-content">
             <div class="back-btn">
-                <a href="matches">
+                <a href="{{ route('leagueMatches',['leagueId'=>$matchDetails->league_id]) }}">
                     <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"
                         fill="#fff">
                         <path d="M360-240 120-480l240-240 56 56-144 144h568v80H272l144 144-56 56Z"></path>
